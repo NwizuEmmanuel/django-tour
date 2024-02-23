9ceb5dab1b119cf2921b6be73f57510b29a66640
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-02-23 14:06:47 +0800

    added detail view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
+    # ex. /polls/
     path("", views.index, name="index"),
+    # ex. /4/detail/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex. /2/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex. /9/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question, Choice
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This is index page.</h1>")
+    latest_question = Question.objects.order_by("pub_date")[:5]
+    context = {"latest_question": latest_question}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    responds = "You are looking at result of question %s."
+    return HttpResponse(responds % question_id)
+
+
+def vote(request, question_id):
+    response = "You are voting on question %s."
+    return HttpResponse(response % question_id)
